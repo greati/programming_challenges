92cd5dd861890522605a4666607a9ee0c5879139
Vitor Greati <greati@ufrn.edu.br>
2016-07-12 15:51:41 -0300

    Ended 706

--- 706/706.cpp
+++ 706/706.cpp
@@ -4,48 +4,87 @@
 
 using namespace std;
 
+int n;
+
+const int UPPER = 0;
+const int SEC_ESQ = 1;
+const int SEC_DIR = 2;
+const int MID = 3;
+const int FOUR_ESQ = 4;
+const int FOUR_DIR = 5;
+const int LAST = 6;
+
+char BLANK = ' ';
+
+bool sections[10][7] = {
+	{true, true, true, false, true, true, true},
+	{false, false, true, false, false, true, false},
+	{true, false, true, true, true, false, true},
+	{true, false, true, true, false, true, true},
+	{false, true, true, true, false, true, false},
+	{true, true, false, true, false, true, true},
+	{true, true, false, true, true, true, true},
+	{true, false, true, false, false, true, false},
+	{true, true, true, true, true, true, true},
+	{true, true, true, true, false, true, true}
+};
+
+string s;
+
+void printVertical(int partEsq, int partDir) {
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < s.length(); ++j) {
+			int a = s[j] - '0';
+			if (sections[a][partEsq])
+				cout << "|";
+			else 
+				cout << BLANK;
+			for (int k = 0; k < n; k++)
+				cout << BLANK;
+			if (sections[a][partDir])
+				cout << "|";
+			else
+				cout << BLANK;
+			if (j < s.length() - 1)
+				cout << BLANK;
+		}
+		cout << endl;
+	}
+}
+
+void printHorizontal(int part) {
+	for (int i = 0; i < s.length(); ++i) {
+		int a = s[i] - '0';
+		cout << BLANK;
+		for (int k = 0; k < n; k++)
+			if (sections[a][part])
+				cout << "-";
+			else cout << BLANK;
+		if (i < s.length() - 1)
+			cout << BLANK;
+		cout << BLANK;
+	}	
+	cout << endl;
+}	
+
 int main(void) {
-
-	int n;
-	char lcd[23][8 * 12];
-
-	const int UPPER = 0;
-	const int SEC_ESQ = 1;
-	const int SEC_DIR = 2;
-	const int MID = 3;
-	const int FOUR_ESQ = 4;
-	const int FOUR_DIR = 5;
-	const int LAST = 6;
-
-	bool sections[10][7] = {
-		{false, false, true, false, false, true, false},
-		{true, false, true, true, true, false, true},
-		{true, false, true, true, false, true, true},
-		{false, true, true, true, false, true, false},
-		{true, true, false, true, false, true, true},
-		{true, true, false, true, true, true, true},
-		{true, false, true, false, false, true, false},
-		{true, true, true, true, true, true, true},
-		{true, true, true, true, false, true, true},
-		{true, true, true, false, true, true, true}
-	};
-
 	while (true) {
-	
 		cin >> n;
 
 		if (n == 0) return 0;
 
-		string s;
-		
 		cin >> s;
-
-		int uli = 0; // upper left index
-
-		for (int i = 0; i < s.length(); ++i) {
-			int a = s[i] - '0';
-		}		
+		
+		printHorizontal(UPPER);
+		printVertical(SEC_ESQ, SEC_DIR);
+		printHorizontal(MID);
+		printVertical(FOUR_ESQ, FOUR_DIR);
+		printHorizontal(LAST);
+	
+		cout << endl;
 	}
 
 	return 0;
 }
+
+
