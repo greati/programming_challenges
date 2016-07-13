da346aed42f2cdc6fc39c0ff22326754429cdb06
Vitor Greati <greati@ufrn.edu.br>
2016-07-13 01:28:17 -0300

    10267

--- 10267/10267.cpp
+++ 10267/10267.cpp
@@ -1,17 +1,72 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-char image[251][251];
+char image[255][255];
+
+int delta[4][2] = {{1,0},{0,1},{-1,0},{0,-1}};
+
+void initialize(int m, int n){
+	for (int i = 1; i <= n; ++i)
+		for (int j = 1; j <= m; ++j)
+			image[i][j] = 'O';
+}
+
+void drawVert(int x, int y1, int y2, char color) {
+	if (y1 > y2)
+		swap(y1,y2);
+	for (int i = y1; i <= y2; ++i)
+		image[i][x] = color;
+}
+
+void drawHoriz(int y, int x1, int x2, char color) {
+	if (x1 > x2)
+		swap(x1,x2);
+	for (int i = x1; i <= x2; ++i)
+		image[y][i] = color;
+}
+
+void drawRect(int x1, int y1, int x2, int y2, char color){
+	for (int i = y1; i <= y2; ++i)
+		for (int j = x1; j <= x2; ++j)
+			image[i][j] = color;
+}
+
+void save(string name, int m, int n) {
+	cout << name << endl;
+	for (int i = 1; i <= n; ++i) {
+		for (int j = 1; j <= m; ++j) {
+			cout << image[i][j]; 
+		}	
+		cout << endl;
+	}
+	cout << endl;
+}
+
+void colourRegion(int x, int y, int cols, int rows, char colorOriginal, char newColor){
+	if (x >= cols+1 or y >= rows+1 or x <= 0 or y <= 0)
+		return;
+	if (image[y][x] != colorOriginal)
+		return;
+	for (int i = 0; i < 4; ++i) {
+		int xx = x + delta[i][0];
+		int yy = y + delta[i][1];
+		colourRegion(xx, yy, cols, rows, colorOriginal, newColor);			
+	}
+	image[y][x] = newColor;	
+}
 
 int main(void) {
 
+	int m, n;
+	
 	while (true) {
 
 		char op = 'I';
-		int m, n;
 		int x1, x2, y1, y2;
 		char color;
+		string name;
 
 		do {
 			cin >> op;
@@ -26,7 +81,7 @@ int main(void) {
 					break;
 				case 'L':
 					cin >> x1 >> y1 >> color;
-					image[x1][y1] = color;
+					image[y1][x1] = color;
 					break;
 				case 'V':
 					cin >> x1 >> y1 >> y2 >> color;
@@ -41,9 +96,10 @@ int main(void) {
 					drawRect(x1,y1,x2,y2,color);
 					break;
 				case 'F':
+					cin >> x1 >> y1 >> color;
+					colourRegion(x1,y1,m,n,image[y1][x1],color);
 					break;
 				case 'S':
-					string name;
 					cin >> name;
 					save(name,m,n);
 					break;
@@ -51,6 +107,7 @@ int main(void) {
 					return 0;
 					break;
 				default:
+					continue;
 			}
 			
 		} while (op != 'S');
@@ -58,36 +115,3 @@ int main(void) {
 	return 0;
 }
 
-void initialize(int m, int n){
-	for (int i = 0; i < m; ++i)
-		for (int j = 0; j < n; ++j)
-			image[i][j] = 'O';
-}
-
-void drawVert(int x, int y1, int y2, char color) {
-	for (int i = y1; i < y2; ++i)
-		image[x][i] = color;
-}
-
-void drawHoriz(int y, int x1, int x2, char color) {
-	for (int i = x1; i < x2; ++i)
-		image[i][y] = color;
-}
-
-void drawRect(int x1, int y1, int x2, int y2, char color){
-	for (int i = x1; i < x2; ++i)
-		for (int j = y1; j < y2; ++j)
-			image[i][j] = color;
-}
-
-void save(string name, int m, int n) {
-	cout << name << endl;
-	for (int i = 0; i < m; ++i)
-		for (int j = 0; j < n; ++j)
-			cout << image[i][j]; 	
-	cout << endl;
-}
-
-void colourRegion(int x, int y, char color){
-
-}
