a713cfb6ce3cd2098a42f2d0a0ce06f3cccd63c2
Vitor Greati <vitorgreati@hotmail.com>
2016-10-18 14:29:35 -0300

    FIM 2237

--- uri/2237/2237All.cpp
+++ uri/2237/2237All.cpp
@@ -2,15 +2,11 @@
 
 using namespace std;
 
-map<int,vector<int>> idToV;
+vector<int> idToV[40321];
 map<vector<int>, int> vToId;
 vector<pair<int,int>> adj[40321]; 
-map<int,int> minDists;
-map<int,bool> vis;
-
-//map<vector<int>,int> minDists;
-//map<vector<int> , vector< pair<int,vector<int>> > > adj;
-//map<vector<int>,bool> vis;
+int minDists[40321];
+bool vis[40321];
 
 priority_queue<pair<int, int>, vector< pair<int,int> >, greater<pair<int,int>>> pq;
 
@@ -44,34 +40,36 @@ int main(void) {
 	sort(permut.begin(), permut.end());
 	int pId = 0;
 	do {
-		minDists.insert(make_pair(pId, (1<<30)));	
-		vis.insert(make_pair(pId, false));
-		idToV.insert(make_pair(pId,permut)); //*
+		minDists[pId] = (1<<30);
+		vis[pId] = false;
+		//minDists.insert(make_pair(pId, (1<<30)));	
+		//vis.insert(make_pair(pId, false));
+		//idToV.insert(make_pair(pId,permut)); //*
+		idToV[pId] = permut;
 		vToId.insert(make_pair(permut, pId++)); //*
 	} while (next_permutation(permut.begin(), permut.end()));
 
 	int idInitial = vToId[initial];
 	int idGoal = vToId[goal];
 
-	// Edges
-	for (auto it = minDists.begin(); it != minDists.end(); ++it) {
-		//vector<pair<int,vector<int>>> newAdj;
-		//adj.insert(make_pair(it->first, newAdj));
-		// make edges
-		make_edge(it->first, 0, 1);	
-		make_edge(it->first, 1, 2);	
-		make_edge(it->first, 2, 3);	
-		make_edge(it->first, 4, 5);	
-		make_edge(it->first, 5, 6);	
-		make_edge(it->first, 6, 7);	
-		make_edge(it->first, 0, 4);	
-		make_edge(it->first, 1, 5);	
-		make_edge(it->first, 2, 6);	
-		make_edge(it->first, 3, 7);	
-	}
 
+	// Edges
+	for (int it = 0; it < pId; ++it) {
+		// make edges
+		make_edge(it, 0, 1);	
+		make_edge(it, 1, 2);	
+		make_edge(it, 2, 3);	
+		make_edge(it, 4, 5);	
+		make_edge(it, 5, 6);	
+		make_edge(it, 6, 7);	
+		make_edge(it, 0, 4);	
+		make_edge(it, 1, 5);	
+		make_edge(it, 2, 6);	
+		make_edge(it, 3, 7);	
+	}
 	minDists[idInitial] = 0;
 	pq.push(make_pair(0, idInitial));
+
 
 	// Dijkstra
 	pair<int, int> minEdge;	
@@ -92,7 +90,7 @@ int main(void) {
 		}
 		vis[v] = true;
 		pq.pop();	
-	}
+	} 
 
 	cout << minDists[idGoal] << endl;
 
