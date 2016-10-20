c6bac9210132c9e989b25358543755f7dc58acc2
Vitor Greati <greati@ufrn.edu.br>
2016-10-19 23:09:54 -0300

    1886 done

--- uri/1886/1886.cpp
+++ uri/1886/1886.cpp
@@ -1,49 +1,55 @@
 #include <bits/stdc++.h>
-
 using namespace std;
+
+bool vis[1001];
+int sets[2];
+int vertexSet[1001];
+vector<int> adj[1001];
+queue<int> q;
+int remaining;
 
 int main(void) {
 	int t;
-
+	int n,m;
 	scanf("%d", &t);
-
-	set<int> A,B;
-
-	bool vis[1001];
-
 	while (t--) {
-		int n, m;
-		int ans = 0;
-
+		q.clear();
 		scanf("%d %d",&n,&m);
-
-		for (int i = 0; i <= n; ++i)
+		// Cleaning
+		for (int i = 0; i <= n; ++i) {
 			vis[i] = false;
-
+			adj[i].clear();
+		}
+		sets[0] = sets[1] = 0;
+		// Read graph
 		for (int i = 0; i < m; ++i) {
 			int u,v;
 			scanf("%d %d", &u, &v);
-			if (!vis[u] && !vis[v]) {
-				ans++;
-				vis[u] = vis[v] = true;
-			}
-						
-			/*if (A.find(u) != A.end()) {
-				B.insert(v);
-			} else if (B.find(u) != B.end()) {
-				A.insert(v);
-			} else if (A.find(v) != A.end()) {
-				B.insert(u);
-			} else if (B.find(v) != B.end()) {
-				A.insert(u);
-			} else {
-				A.insert(u); B.insert(v);
-			}*/	
+			adj[u].push_back(v);
+			adj[v].push_back(u);			
 		}	
-		cout << ans + (n-2*ans) << endl;
-		//printf("%d\n", max(A.size(), B.size()));
-		//A.clear(); B.clear();
+		// BFS
+		sets[0]++;
+		vertexSet[1] = 0;
+		q.push(1);
+		vis[1] = true;
+		remaining = n-1;
+		while (!q.empty()) {
+			int cur = q.front();
+			int scur = vertexSet[cur];
+			for (int i = 0; i < adj[cur].size(); ++i) {
+				if (!vis[adj[cur][i]]) {
+					vis[adj[cur][i]] = true;
+					q.push(adj[cur][i]);
+					sets[1 - scur]++; 	
+					vertexSet[adj[cur][i]] = 1 - scur;
+					remaining--;
+				}
+			}
+			q.pop();	
+			if (remaining == 0) break;
+		}
+		printf("%d\n", max(sets[0],sets[1]));
 	}
-
 	return 0;
 }
