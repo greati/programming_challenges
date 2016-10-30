8acf14c36c634ea3e44dff0c8da0b5894b6b6f8d
Vitor Greati <greati@ufrn.edu.br>
2016-10-30 13:26:21 -0300

    670 problems

---
+++ 670/670.cpp
@@ -0,0 +1,163 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Point {
+	int id, x, y;
+	Point(){};
+	Point(int _x, int _y, int _id) : x {_x}, y {_y}, id {_id}{}; 
+};
+
+double euclidean(Point b1, Point b2) {
+	return sqrt((double) ((b2.x - b1.x) * (b2.x - b1.x) + (b2.y - b1.y) * (b2.y - b1.y)));
+}
+
+bool reacheable(Point b1, Point b2, Point r) {
+	return euclidean(b1,r) + euclidean(b2,r) <= 2 * euclidean(b1,b2);	
+}
+
+struct Edge {
+	Point p;
+	int residual;
+	Edge * opposite;
+	Edge() {};
+	Edge(Point _p) : p {_p}, residual {1}, opposite {nullptr} {};
+	Edge(Point _p, int w) : p {_p}, residual {w}, opposite {nullptr} {};
+};
+
+vector<Edge> graph[207];
+bool vis[207];
+int parent[207];
+Edge* path[207];
+
+/* Para cada ponto i de bob, guarda
+ * um lugar interessante visitado,
+ * de forma que visitedPlaces[i]
+ * retorna o ponto interessante visitado
+ * a partir de i. Com ele, é possível
+ * imprimir todos os pontos da rota.
+ * **/
+Point * visitedPlaces[207];
+
+int flow = 0;
+
+int updateNetwork(int vertex, int minCapacity){
+	if (parent[vertex] != -1) {
+		Edge * edgeInPath = path[vertex];
+		int m = updateNetwork(parent[vertex], min(minCapacity, edgeInPath->residual));			
+		//edgeInPath->residual -= m;
+		edgeInPath->residual = 0;
+		if (edgeInPath->opposite != nullptr)
+			//((edgeInPath->opposite)->residual) += m;
+			((edgeInPath->opposite)->residual) = 1;
+		return m;
+	}
+	return minCapacity;
+}
+
+int main(void) {
+
+	int L, m, n, _x, _y;
+
+	scanf("%d", &L);
+
+	Point bobs[101];
+	Point ralphs[101];
+
+	int s = 205, t = 206;
+
+	Point epp {1000,1000, t};
+	Edge et {epp};
+
+	queue<int> q;
+
+	while (L--) {
+
+		for (int i = 0; i < 207; ++i) {
+			graph[i].clear();
+			visitedPlaces[i] = nullptr;
+		}
+
+		scanf("%d%d", &n, &m);	
+	
+		for (int i = 0; i < n; ++i) {
+			scanf("%d%d", &_x, &_y);
+			Point p {_x, _y, i};
+			bobs[i] = p;
+			Edge e {p};
+			graph[s].push_back(e);
+		}	
+	
+		for (int i = 0; i < m; ++i) {
+			scanf("%d%d", &_x, &_y);
+			Point p {_x, _y, n + i};
+			ralphs[i] = p;
+			Edge e {p};
+			graph[p.id].push_back(et);
+		}
+
+		for (int i = 0; i < n - 1; ++i) {
+			Point b1 = bobs[i];	
+			Point b2 = bobs[i + 1];	
+			for (int j = 0; j < m; ++j) {
+				if (reacheable(b1,b2,ralphs[j])) {
+					Edge e {ralphs[j]};
+					Edge ep {bobs[i], 0};
+					e.opposite = &ep;
+					ep.opposite = &e;
+					graph[i].push_back(e);
+					graph[n + j].push_back(ep);
+					//printf("%d:%d,%d  %d:%d,%d\n", b1.id, b1.x, b1.y, ralphs[j].id, ralphs[j].x, ralphs[j].y);
+				}
+			}
+		}
+		
+		for (int i = 0; i < n + m; ++i) {
+			cout << i << " -> ";
+			for (int j = 0; j < graph[i].size(); ++j) {
+				cout << graph[i][j].p.id << " ";	
+			}
+			cout << endl;
+		}
+	
+		flow = 0;
+		
+		do {
+			for (int i = 0; i < 207; ++i) {
+				vis[i] = false;
+				parent[i] = -1;
+			}
+			q.push(s);
+			vis[s] = true;
+			while (!q.empty()) {
+				int cur = q.front();
+				for (int i = 0; i < graph[cur].size(); ++i) {
+					Edge * e = &graph[cur][i];
+					if (!vis[e->p.id] && (e->residual) > 0) {
+						q.push(e->p.id);
+						vis[e->p.id] = true;	
+						path[e->p.id] = e;
+						parent[e->p.id] = cur;
+					}
+				}	
+				q.pop();
+			}			
+
+			if(vis[t]) {
+				flow += updateNetwork(t,1<<30);
+				visitedPlaces[parent[path[parent[t]]->p.id]] = &ralphs[path[parent[t] - n]->p.id];
+			}
+
+		} while (vis[t]);	
+
+		printf("%d\n", n + flow);
+		for (int i = 0; i < n; ++i) {
+			printf(i != (n-1) ? "%d %d " : "%d %d", bobs[i].x, bobs[i].y);
+			if (visitedPlaces[i] != nullptr) {
+				printf("%d %d ", visitedPlaces[i]->x, visitedPlaces[i]->y);
+			}
+		}
+		printf("\n\n");
+		
+	}	
+}
--- 820/820.cpp
+++ 820/820.cpp
@@ -1,28 +1,34 @@
-#include <bits/stdc++.h>
+#include <stdio.h>
+#include <iostream>
+#include <queue>
+#include <algorithm>
 
 using namespace std;
 
 struct Edge {
 	int vertex;
-	int capacity;
-	int flow;
 	int residual;
-	Edge (int v, int c) : vertex {v}, capacity {c}, flow{0}, residual {c} {};
+	Edge * opposite;
+	Edge (int v, int c) : vertex {v}, residual {c},opposite{nullptr} {};
 	Edge(){};
 };
 
 vector<Edge> graph[101];
-vector<bool> vis;
-vector<int> parent;
-vector<Edge> path;
+bool vis[101];
+int parent[101];
+Edge* path[101];
+int flow = 0;
 
+/**
+ * Returns the weight of the critical edge and updates
+ * the residual graph.
+ * */
 int updateNetwork(int vertex, int minCapacity){
 	if (parent[vertex] != -1) {
-		Edge edgeInPath = path[vertex];
-		int m = updateNetwork(parent[vertex], min(minCapacity, edgeInPath.residual));			
-		// Take care on update TODO
-		edgeInPath.residual -= m;
-		edgeInPath.flow += m;
+		Edge * edgeInPath = path[vertex];
+		int m = updateNetwork(parent[vertex], min(minCapacity, edgeInPath->residual));			
+		edgeInPath->residual -= m;
+		((edgeInPath->opposite)->residual) += m;
 		return m;
 	}
 	return minCapacity;
@@ -31,54 +37,62 @@ int updateNetwork(int vertex, int minCapacity){
 int main(void) {
 
 	int n, s, t, c;
-	
-	vis.resize(101); parent.resize(101); path.resize(101);
+
+	int netNumber = 0;
+
+	queue<int> q;
 
 	while (true) {
 		scanf("%d", &n);
 
 		if (n == 0) break;
+	
+		++netNumber;
 
 		scanf("%d%d%d", &s, &t, &c); // source, destination, |E|
 
 		// Clear graph
-		for (int i = 0; i <= n; ++i)	graph[i].clear();
+		for (int i = 0; i <= n; ++i) graph[i].clear();
 
 		// Mounting the network
 		for (int i = 0; i < c; ++i) {
 			int u, v, w;
 			scanf("%d%d%d", &u, &v, &w); // from, to, weight 
-			Edge ep {v, w};
-			Edge en {u, w};
+			Edge ep {v, w}; Edge en {u, w};
+			ep.opposite = &en; en.opposite = &ep;
 			graph[u].push_back(ep);
 			graph[v].push_back(en);
 		}
 
+		flow = 0;
+
 		do {
-			queue<int> q;
 			for (int i = 0; i <= n; ++i) {
 				vis[i] = false;
 				parent[i] = -1;
 			}
-			parent[s] = -1;
 			q.push(s);
+			vis[s] = true;
 			while (!q.empty()) {
 				int cur = q.front();
-				vis[cur] = true;
-				for (int i = 0; i < graph[cur].size(); ++i) {
-					Edge adj = graph[cur][i];	
-					if (!vis[adj.vertex]) {
-						parent[adj.vertex] = cur;
-						path[adj.vertex] = adj;
-						q.push(adj.vertex);
+				for (unsigned int i = 0; i < graph[cur].size(); ++i) {
+					Edge * adj = &graph[cur][i];	
+					if (!vis[adj->vertex] && (adj->residual) > 0) {
+						vis[adj->vertex] = true;
+						parent[adj->vertex] = cur;
+						path[adj->vertex] = adj;
+						q.push(adj->vertex);
 					}
 				}	
 				q.pop();
 			}
 			
-			if (vis[t]) 	updateNetwork(t);
+			if (vis[t])     flow +=	updateNetwork(t, 1001);
 
 		} while (vis[t]);
+
+		printf("Network %d\nThe bandwidth is %d.\n\n", netNumber, flow);
 	}
+
 	return 0;
 }
