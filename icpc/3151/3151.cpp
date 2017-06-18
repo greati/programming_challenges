cf5a907ff6ab5352caced7ce53ffb35ef16216e0
Vitor Greati <greati@ufrn.edu.br>
2017-06-18 18:58:08 -0300

    3151 done

--- icpc/3151/3151.cpp
+++ icpc/3151/3151.cpp
@@ -5,66 +5,44 @@ using namespace std;
 int main(void) {
 
 	int N;
-	double F;
+	float F;
 	int areas[201];
 	int sum[201][201];
 	double pd[201][201];
+	double res = 1000000000;
 
 	while (true) {
-		cin >> N >> F;
+		scanf("%d%f", &N, &F);
 		
 		if (N == F and N == 0) break;
 
-		int total = 0;
+		res = 10000000000;
+
 		for (int i = 0; i < N; ++i) {
-			cin >> areas[i];	
-			total += areas[i];
+			scanf("%d", (areas + i));	
+			sum[i][1] = areas[i];
 		}
 
-		// Compute sums
-		for (int i = 0; i < N; ++i) {
-			sum[i][i] = areas[i];
-			for (int j = i + 1; j < N; ++j)
-				sum[i][j] = sum[i][j-1] + areas[j];
-		}
+		for (int j = 2; j <= N; ++j)
+			for (int i = 0; i < N; ++i)
+				sum[i][j] = sum[i][1] + sum[(i+1)%N][j-1];
 
-		for (int i = 1; i < N; ++i) 
-			for (int j = 0; j < i; ++j) 
-				sum[i][j] = sum[i][(j + (N-1)) % N] + areas[j];
-		
+		for (int i = 0; i < N; ++i)
+			pd[i][1] = 0;
 
-		for (int j = 0; j < N; ++j) {
-			pd[j][j] = 0;
-			for (int i = j-1; i >= 0; --i) {
+		for (int j = 2; j <= N; ++j) {
+			for (int i = 0; i < N; ++i) {
 				pd[i][j] = 10000000;
-				for (int k = i; k < j; ++k) {
-					pd[i][j] = std::min(pd[i][j], pd[i][k%N] + pd[(k+1)%N][j] + F*std::max(sum[i][k%N],sum[(k+1)%N][j]));
+				for (int k = 1; k < j; ++k) {
+					pd[i][j] = std::min(pd[i][j], pd[i][k] + pd[(i+k)%N][j-k] + std::max(sum[i][k], sum[(i+k)%N][j-k]));
 				}
 			}
 		}
-		// PD
-		/*for (int i = 0; i < N; ++i) {
-			for (int j = 0; j < N; ++j) {
-				if (j == i) { pd[i][j] = 0; continue;}
-				pd[i][j] = 10000000;
-				for (int k = i; k < j + N; ++k) {
-					pd[i][j] = std::min(pd[i][j], pd[i][k%N] + pd[(k+1)%N][j] + F*std::max(sum[i][k%N],sum[(k+1)%N][j]));
-				}
-			}
-		}*/
 
-		for (int i = 0; i < N; ++i) {
-			for (int j = 0; j < N; ++j) {
-				std::cout << sum[i][j] << " ";
-			}
-			std::cout << std::endl;
-		}
-		for (int i = 0; i < N; ++i) {
-			for (int j = 0; j < N; ++j) {
-				std::cout << pd[i][j] << " ";
-			}
-			std::cout << std::endl;
-		}
+		for (int i = 0; i < N; ++i)
+			res = std::min(res, pd[i][N]);
+
+		printf("%.2f\n", res * F);
 
 	}
 	return 0;
